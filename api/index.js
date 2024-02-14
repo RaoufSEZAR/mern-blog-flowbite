d8edd09fa78f0558b23cfd79c7576b941408ade0
raoufsezar <raoufsato999@gmail,com>
2024-02-14 22:36:48 +0300

    add error handler and auth

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 
 import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 config();
 
@@ -13,4 +18,22 @@ mongoose
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Internal Server Error";
+	res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
+});
