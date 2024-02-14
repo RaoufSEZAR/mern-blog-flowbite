f30efcfaf5cc3e8b4b02c71d02602d1bd0da906a
raoufsezar <raoufsato999@gmail,com>
2024-02-14 21:14:52 +0300

    add models

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema(
+	{
+		userId: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		image: {
+			type: String,
+			default:
+				"https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png",
+		},
+		category: {
+			type: String,
+			default: "uncategorized",
+		},
+		slug: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Post = model("Post", postSchema);
+
+export default Post;
---
+++ api/models/user.model.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		profilePicture: {
+			type: String,
+			default:
+				"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
