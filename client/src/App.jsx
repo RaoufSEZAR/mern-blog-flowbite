c6124e697aff6ea4726b2e174955e048958599f5
raoufsezar <raoufsato999@gmail,com>
2024-02-15 20:12:01 +0300

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import FooterCom from "./components/Footer";
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/sign-up" element={<Signup />} />
 			</Routes>
+			<FooterCom />
 		</BrowserRouter>
 	);
 }
