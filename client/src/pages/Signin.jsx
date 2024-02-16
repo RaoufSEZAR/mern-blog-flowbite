663852a7c385494c0ad06a8a8c5786ab36aea108
raoufsezar <raoufsato999@gmail,com>
2024-02-16 22:42:30 +0300

    add oauth func

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,6 +7,7 @@ import {
 	signInStart,
 	signInSuccess,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 const Signin = () => {
 	const [formData, setFormData] = useState({});
@@ -93,6 +94,7 @@ const Signin = () => {
 								"Sign In"
 							)}
 						</Button>
+						<OAuth />
 					</form>
 					<div className="flex gap-2 text-sm mt-3 justify-center">
 						<span>Don't have an account?</span>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 const Signup = () => {
 	const [formData, setFormData] = useState({});
@@ -97,6 +98,7 @@ const Signup = () => {
 								"Sign Up"
 							)}
 						</Button>
+						<OAuth />
 					</form>
 					<div className="flex gap-2 text-sm mt-3 justify-center">
 						<span>Have an account?</span>
