c19cfd8d6d148358796354f4d222aa95e0ee7261
raoufsezar <raoufsato999@gmail,com>
2024-02-21 23:14:46 +0300

    show comments inside post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Alert, Button, Textarea } from "flowbite-react";
+import Comment from "./Comment";
 
 // eslint-disable-next-line react/prop-types
 const CommentSection = ({ postId }) => {
 	const { currentUser } = useSelector((state) => state.user);
 	const [comment, setComment] = useState("");
 	const [commentError, setCommentError] = useState(null);
+	const [comments, setComments] = useState([]);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -33,10 +35,27 @@ const CommentSection = ({ postId }) => {
 			}
 			setComment("");
 			setCommentError(null);
+			setComments([data, ...comments]);
 		} catch (error) {
 			setCommentError(error.message);
 		}
 	};
+
+	useEffect(() => {
+		const getcomments = async () => {
+			try {
+				const res = await fetch(`/api/comment/getPostComments/${postId}`);
+				if (res.ok) {
+					const data = await res.json();
+					setComments(data);
+				}
+			} catch (error) {
+				console.log(error.message);
+			}
+		};
+
+		getcomments();
+	}, [postId]);
 	return (
 		<div className="mx-auto max-w-2xl w-full p-3">
 			{currentUser ? (
@@ -89,6 +108,21 @@ const CommentSection = ({ postId }) => {
 						</Alert>
 					)}
 				</form>
+			)}
+			{comments.length === 0 ? (
+				<p className="text-sm text-teal-500 my-5">No comments yet!</p>
+			) : (
+				<>
+					<div className="text-sm my-5 flex items-center gap-1">
+						<p>Comments</p>
+						<div className="border border-gray-400 py-1 px-2 rounded-sm">
+							<p>{comments.length}</p>
+						</div>
+					</div>
+					{comments.map((comment) => (
+						<Comment comment={comment} key={comment._id} />
+					))}
+				</>
 			)}
 		</div>
 	);
