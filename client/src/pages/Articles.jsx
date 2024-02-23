584308b2dfce3d2a48677c6966bf14190398d521
raoufsezar <raoufsato999@gmail,com>
2024-02-23 23:13:26 +0300

    add more localization

--- client/src/pages/Articles.jsx
+++ client/src/pages/Articles.jsx
@@ -1,13 +1,12 @@
+import { useTranslation } from "react-i18next";
 import CallToAction from "../components/CallToAction";
 
 export default function Articles() {
+	const { t } = useTranslation();
 	return (
-		<div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
-			<h1 className="text-3xl font-semibold">Articles</h1>
-			<p className="text-md text-gray-400">
-				Build fun and engaging projects while learning HTML, CSS, and
-				JavaScript!
-			</p>
+		<div className="min-h-screen max-w-4xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+			<h1 className="text-3xl font-semibold">{t("articles")}</h1>
+			<p className="text-md text-gray-400">{t("find-more-articles")}</p>
 			<CallToAction />
 		</div>
 	);
